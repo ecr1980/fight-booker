a889674cf970a97b74ef73bd97eb0071564cf50a
Eric Romano <ecr1980@gmail.com>
2022-05-05 11:46:31 -0400

    Added model relationships.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,5 @@
 class Flight < ApplicationRecord
+  belongs_to :departing_airport, class_name: "Airport"
+  belongs_to :arriving_airport, class_name: "Airport"
+  has_and_belongs_to_many :passengers, class_name: "User"
 end
